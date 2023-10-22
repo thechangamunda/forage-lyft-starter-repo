60b18314a6d55be275622b8e0f929ec3fd70c24e
Yuvraj Joshi <yuvrajjoshi2003@gmail.com>
2023-10-22 16:02:39 +0530

    task 4

--- battery/battery_type/spindler_battery.py
+++ battery/battery_type/spindler_battery.py
@@ -5,7 +5,7 @@ class SpindlerBattery(Battery):
     def __init__(self, current_date, last_service_date):
         self.current_date = current_date
         self.last_service_date = last_service_date
-        self.spindler_year_need_service = 4
+        self.spindler_year_need_service = 3
 
     def needs_service(self):
         date_need_service = self.last_service_date.replace(year=self.last_service_date.year + self.spinder_year_need_service)
